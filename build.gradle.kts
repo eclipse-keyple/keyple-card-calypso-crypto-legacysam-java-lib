ea0506cb53804afa4b0d735b2c3b81269a93e694
Andrei Cristea <andrei.cristea019@gmail.com>
2023-08-17 10:06:32 +0200

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("org.eclipse.keypop:keypop-reader-java-api:2.0.0-SNAPSHOT") { isChanging = true }
     implementation("org.eclipse.keypop:keypop-card-java-api:2.0.0-SNAPSHOT") { isChanging = true }
     implementation("org.eclipse.keypop:keypop-calypso-card-java-api:2.0.0-SNAPSHOT") { isChanging = true }
-    implementation("org.eclipse.keypop:keypop-calypso-crypto-symmetric-java-api:1.0.0-SNAPSHOT") { isChanging = true }
+    implementation("org.eclipse.keypop:keypop-calypso-crypto-symmetric-java-api:0.1.0-SNAPSHOT") { isChanging = true }
     implementation("org.eclipse.keypop:keypop-calypso-crypto-legacysam-java-api:1.0.0-SNAPSHOT") { isChanging = true }
     implementation("org.eclipse.keyple:keyple-common-java-api:2.0.0")
     implementation("org.eclipse.keyple:keyple-service-resource-java-lib:3.0.0-SNAPSHOT") { isChanging = true }
