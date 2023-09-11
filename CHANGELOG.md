e0dbd5aef32606fdecbf983b5318b03a84bf41bc
Andrei Cristea <andrei.cristea019@gmail.com>
2023-09-11 13:57:44 +0200

    fix: add S1D3 to S1D7 to the list of SAM types recognized by the library

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ### Added
+- Added S1D3 to S1D7 to the list of SAM types recognized by the library.
 - Added project status badges on `README.md` file.
 ### Fixed
 - CI: code coverage report when releasing.
+### Upgraded
+- Calypsonet Terminal Reader API `1.2.0` -> `1.3.0` (source code not impacted)
+- Keyple Util Library `2.3.0` -> `2.3.1` (source code not impacted)
 
 ## [0.3.0] - 2023-02-27
 ### Upgraded
--- build.gradle.kts
+++ build.gradle.kts
@@ -30,12 +30,12 @@ repositories {
     maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
 }
 dependencies {
-    implementation("org.calypsonet.terminal:calypsonet-terminal-reader-java-api:1.2.0")
+    implementation("org.calypsonet.terminal:calypsonet-terminal-reader-java-api:1.3.0")
     implementation("org.calypsonet.terminal:calypsonet-terminal-card-java-api:1.0.0")
     implementation("org.calypsonet.terminal:calypsonet-terminal-calypso-crypto-legacysam-java-api:0.2.0")
     implementation("org.eclipse.keyple:keyple-common-java-api:2.0.0")
     implementation("org.eclipse.keyple:keyple-service-resource-java-lib:2.0.2")
-    implementation("org.eclipse.keyple:keyple-util-java-lib:2.3.0")
+    implementation("org.eclipse.keyple:keyple-util-java-lib:2.3.1")
     implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("org.slf4j:slf4j-simple:1.7.32")
