0e01b355f0574084cc3a169876e62394f70684ef
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-11-21 11:26:30 +0100

    fix(dependencies): update SNAPSHOT versions to stable releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,15 +14,11 @@ plugins {
 ///////////////////////////////////////////////////////////////////////////////
 
 dependencies {
-  implementation("org.eclipse.keypop:keypop-reader-java-api:2.1.0-SNAPSHOT") { isChanging = true }
+  implementation("org.eclipse.keypop:keypop-reader-java-api:2.1.0")
   implementation("org.eclipse.keypop:keypop-card-java-api:2.0.1")
-  implementation("org.eclipse.keypop:keypop-calypso-card-java-api:2.2.0-SNAPSHOT") {
-    isChanging = true
-  }
+  implementation("org.eclipse.keypop:keypop-calypso-card-java-api:2.2.0")
   implementation("org.eclipse.keypop:keypop-calypso-crypto-symmetric-java-api:0.1.1")
-  implementation("org.eclipse.keypop:keypop-calypso-crypto-legacysam-java-api:1.0.0-SNAPSHOT") {
-    isChanging = true
-  }
+  implementation("org.eclipse.keypop:keypop-calypso-crypto-legacysam-java-api:1.0.0")
   implementation("org.eclipse.keyple:keyple-common-java-api:2.0.2")
   implementation("org.eclipse.keyple:keyple-service-resource-java-lib:3.1.0")
   implementation("org.eclipse.keyple:keyple-util-java-lib:2.4.0")
